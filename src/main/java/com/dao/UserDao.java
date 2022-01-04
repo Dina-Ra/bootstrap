a8ebaeb90d567c074aa2d51515687ec11668f3ca
Dinara Kutlugildina <mnbvcxz4321@mail.ru>
2022-01-04 20:32:31 +0500

    Initial commit

---
+++ src/main/java/com/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+
+
+import com.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    public void addUser(User user);
+    public void removeUserById(long id);
+    public List<User> getAllUsers();
+    public void updateUser(User user, long id);
+    public User getUser(long id);
+    public User getUser(String name);
+
+}
---
+++ src/main/java/com/service/UserServiceImpl.java
@@ -0,0 +1,72 @@
+package com.service;
+
+import com.dao.UserDao;
+import com.model.Role;
+import com.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+
+    private UserDao daoHibernate;
+
+    @Autowired
+    public UserServiceImpl(UserDao daoHibernate) {
+        this.daoHibernate = daoHibernate;
+    }
+
+    @Transactional
+    @Override
+    public void addUser(User user, String[] roles) {
+        user.setRoles(getSetRoles(roles));
+        daoHibernate.addUser(user);
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        daoHibernate.removeUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<>();
+        userList = daoHibernate.getAllUsers();
+        return userList;
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user, long id, String[] roles) {
+        user.setRoles(getSetRoles(roles));
+        daoHibernate.updateUser(user, id);
+    }
+
+    @Transactional
+    @Override
+    public User getUser(long id){
+        return daoHibernate.getUser(id);
+    }
+
+    @Transactional
+    @Override
+    public User getUser(String name) {
+        return daoHibernate.getUser(name);
+    }
+
+    private Set<Role> getSetRoles (String[] roles) {
+        Set<Role> setRoles = new HashSet<>();
+        for (String role : roles) {
+            setRoles.add(new Role((role.equals("ADMIN") ? 1L : 2l), "ROLE_" + role));
+        }
+        return setRoles;
+    }
+}

6ff87e1aaaf7c42512098ae4f9c840fde2cd1546
Dinara Kutlugildina <mnbvcxz4321@mail.ru>
2022-01-14 02:43:11 +0500

    "bootstrap"

--- src/main/java/com/controller/AdminController.java
+++ src/main/java/com/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.model.Role;
 import com.model.User;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -20,43 +21,48 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
-
     @GetMapping("/")
-    public String getUsers(ModelMap model, Principal principal) {
+    public String getUsers(Principal principal, Model model) {
+        model.addAttribute("user", userService.getUser(principal.getName()));
         model.addAttribute("users", userService.getAllUsers());
-        User user = userService.getUser(principal.getName());
-        model.addAttribute("user", user);
-        return "show_users";
+        return "admin";
     }
 
-    @GetMapping("/{id}")
-    public String showUser(@PathVariable("id") int id, ModelMap model) {
-        User user = userService.getUser(id);
-        model.addAttribute("user", user);
-        List<Role> roles = user.getRoles().stream().collect(Collectors.toList());
-        model.addAttribute("roles", roles);
-        return "/show_user";
-    }
+//    @GetMapping("/")
+//    public String getUsers(ModelMap model, Principal principal) {
+//        model.addAttribute("users", userService.getAllUsers());
+//        User user = userService.getUser(principal.getName());
+//        model.addAttribute("user", user);
+//        return "admin";
+//    }
 
-    @GetMapping("/new")
-    public String newUser(Model model) {
-        User user = new User();
-        model.addAttribute("newUser", user);
-        return "add_user";
-    }
+//    @GetMapping("/{id}")
+//    public String showUser(@PathVariable("id") int id, ModelMap model) {
+//        User user = userService.getUser(id);
+//        model.addAttribute("user", user);
+//        List<Role> roles = user.getRoles().stream().collect(Collectors.toList());
+//        model.addAttribute("roles", roles);
+//        return "show";
+//    }
+
+//    @GetMapping("/new")
+//    public String newUser(Model model) {
+//        User user = new User();
+//        model.addAttribute("user", user);
+//        return "new";
+//    }
 
     @PostMapping("/add")
-    public String addUser(@ModelAttribute("newUser") User user, @ModelAttribute("listRoles") String[] roles) {
-        System.out.println(user);
+    public String addUser(@ModelAttribute("user") User user, @ModelAttribute("listRoles") String[] roles) {
         userService.addUser(user, roles);
         return "redirect:/admin/";
     }
 
-    @GetMapping("/{id}/edit")
-    public String editUser(ModelMap modelMap, @PathVariable("id") long id) {
-        modelMap.addAttribute("user", userService.getUser(id));
-        return "update";
-    }
+//    @GetMapping("/{id}")
+//    public String editUser(ModelMap modelMap, @PathVariable("id") long id) {
+//        modelMap.addAttribute("user", userService.getUser(id));
+//        return "redirect:/admin/";
+//    }
 
     @PatchMapping("/{id}/patch")
     public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id, @ModelAttribute(
@@ -65,7 +71,12 @@ public class AdminController {
         return "redirect:/admin/";
     }
 
-    @DeleteMapping("/{id}")
+//    @GetMapping("/{id}/delete")
+//    public String deleteUser(ModelMap modelMap, @PathVariable("id") long id) {
+//        modelMap.addAttribute("user", userService.getUser(id));
+//        return "delete";
+//    }
+    @DeleteMapping("/{id}/delete")
     public String delete(@PathVariable("id") int id) {
         userService.removeUserById(id);
         return "redirect:/admin/";
